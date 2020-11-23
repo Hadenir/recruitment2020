73adead7640e0967eb62ec74d61aac1791f1f4d4
Hadenir <hadenir@gmail.com>
2020-11-23 18:59:55 +0100

    Remove additional loop

--- basics/12-forloop.cpp
+++ basics/12-forloop.cpp
@@ -10,11 +10,6 @@ int main()
     int n;
     std::cin >> n;
 
-    for(int i = n; i > 0; i--)
-    {
-        std::cout << "Hello " << name << std::endl;
-    }
-
     for(int i = 0; i < n; i++)
     {
         std::cout << "Hello " << name << std::endl;
